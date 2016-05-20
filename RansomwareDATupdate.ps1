f15d4fe128499ea1ad31508dbc3793668dbe4366
TuningIsLife <patrick@tuningislife.com>
2016-05-20 17:09:20 -0400

    Add files via upload

---
+++ RansomwareDATupdate.ps1
@@ -0,0 +1,35 @@
+﻿##############################################
+# "Crypto Canary" Powershell DAT file update script
+# Written 2016-05-20 by P. Gill 
+##############################################
+
+#Get date
+$date = get-date -format yyyyddMM
+
+#Define locations
+$csvFile = "$env:temp\Ransomware.dat$date"
+
+# Create Excel object by pulling down Google Sheet export
+$objExcel = new-object -comobject excel.application 
+$objExcel.Visible = $False
+$RansomwareOverview = $objExcel.Workbooks.Open("https://docs.google.com/spreadsheets/d/1TWS238xacAto-fLKh1n5uTsdijWdCEsGIM0Y0Hvmc5g/pub?output=xlsx") 
+$ransomwareList = $RansomwareOverview.Worksheets.Item(1)
+
+#Extract the extension list from the second column 
+$rawExtensionsList = @()
+$count = $ransomwareList.Cells.Item(65536, 2).End(-4162)
+for($intRow=2;$intRow -le $count.row;$intRow++)
+{
+	#Generate list from column, and break on new lines
+    $rawExtensionsList += $ransomwareList.Cells.Item($intRow, 2).Value() -split '[\n]'
+    }
+
+#Convert extension list from Object String type and remove blank entries. 
+$extensions = $rawExtensionsList.Split("",[System.StringSplitOptions]::RemoveEmptyEntries) | Select-Object @{Name='Name';Expression={$_}}
+
+#Export extensions to CSV file
+$extensions | Export-Csv -path $csvFile -NoType | ? {$_} 
+
+#Exiting the excel object
+$RansomwareOverview.close($false)
+$objExcel.Quit()
---
+++ crypto_canary_update.ps1
@@ -0,0 +1,44 @@
+﻿##############################################
+# "Crypto Canary" Powershell update script
+# Written 2016-05-20 by P. Gill 
+##############################################
+
+#Get date
+$date = get-date -format yyyyddMM
+
+#Define variables
+$DATFile = "$env:temp\Ransomware.dat$date"
+
+# Import Ransomware DAT file
+$list = Import-Csv $DATFile
+
+#Modify DAT file for each OS and convert to string
+$anyVar='*'
+$08delimChar='|'
+$12delimChar=','
+$12doubleQuotes='"'
+
+[string]$08DAT = get-content $DATFile | select -Skip 1 | %{$_.split('"')[1]} | % {$anyVar+$_+$08delimChar} | Foreach {$_.Trim()}
+[string]$12DAT = get-content $DATFile | select -Skip 1 | %{$_.split('"')[1]} | % {$12doubleQuotes+$anyVar+$_+$12doubleQuotes+$12delimChar} | Foreach {$_.Trim()}
+
+#Drop the last character from each string
+$08DAT = $08DAT -replace ".{1}$"
+$12DAT = $12DAT -replace ".{1}$"
+
+#Detect OS version
+$serverInfo=Get-WmiObject -class Win32_OperatingSystem
+$osVersion=$serverInfo.Version.Substring(0,3)
+
+#Determine correct syntax for OS and update patterns
+#Server 2012 R2
+if ($osVersion -eq "6.3") {	
+
+#Create FSRM File Group
+Set-FsrmFileGroup -Name "Cryptoware" –IncludePattern @($12DAT)
+
+#Server 2008 R2
+} elseif ($osVersion -eq "6.1") {
+
+#Update FSRM File Group
+Filescrn filegroup modify /filegroup:"Cryptoware" /members:"$08DAT"
+}
